13147be8fbbe11f4f12d95c6d1a2feb6f13f2fe0
madinaios <ios@madinagroup.co.tz>
2019-09-20 10:38:10 +0530

    code commited
    code commited

---
+++ FoodieAppNew/FoodieAppNew/Controller/CreateAccountVC.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+    
+    class func initViewController() -> CreateAccountVC {
+        let vc = CreateAccountVC.init(nibName: "CreateAccountVC", bundle: nil)
+        return vc
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+  
+
+}
